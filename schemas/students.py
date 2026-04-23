98952498848e23b831ae4dead27259d7a6549fae
zdr3zdr <zdr3zdr@gmail.com>
2026-04-23 23:24:11 +0300

    change Users, add Stream

--- schemas/students.py
+++ schemas/students.py
@@ -1,28 +1,38 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 from pydantic import BaseModel
 from sqlmodel import Field, Relationship, SQLModel
 
 if TYPE_CHECKING:
     from .references import ReferenceFace
+    from .users import User
 
 
-# TODO GroupBase, GroupCreate, GroupUpdate, GroupPublic, GroupPublicWithStudents
+# TODO StreamBase, StreamCreate, StreamUpdate, StreamPublic, StreamWithGroups, StreamWithGroupsAndStudents
+class Stream(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+
+    groups: list["Group"] = Relationship(back_populates="stream", cascade_delete=True)
+
+
 class GroupBase(SQLModel):
     name: str = Field(index=True)
-    potok: int = Field()
+    stream_id: int = Field(foreign_key="stream.id", ondelete="CASCADE")
 
-class Group(SQLModel, table=True):
+class Group(GroupBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
 
+    stream : "Stream" = Relationship(back_populates="groups")
     students: list["Student"] = Relationship(back_populates="group", cascade_delete=True)
+    user: Optional["User"] = Relationship(back_populates='group')
 
 class GroupCreate(GroupBase):
     pass
 
 class GroupUpdate(BaseModel):
     name: str | None = None
-    potok: int | None = None
+    stream: int | None = None
 
 class GroupPublic(GroupBase):
     id: int
@@ -31,18 +41,17 @@ class GroupPublicWithStudents(GroupPublic):
     students: list["StudentPublic"]
 
 
-# TODO StudentBase, StudentCreate, StudentUpdate, StudentPublic, StudentPublicWithGroup, StudentPublicWithGroupAndReferences
 class StudentBase(SQLModel):
     name: str = Field(index=True)
     group_id: int = Field(foreign_key="group.id", ondelete="CASCADE")
-    telephone_number : str | None = Field()
+    phone_number : str | None = Field()
 
-
-class Student(SQLModel, table=True):
+class Student(StudentBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
 
     group: "Group" = Relationship(back_populates="students")
     references: list["ReferenceFace"] = Relationship(back_populates="student", cascade_delete=True)
+    user: Optional["User"] = Relationship(back_populates="student")
 
 class StudentCreate(StudentBase):
     pass
--- schemas/users.py
+++ schemas/users.py
@@ -1,4 +1,9 @@
+from typing import TYPE_CHECKING, Optional
+
 from sqlmodel import Field, Relationship, SQLModel
+
+if TYPE_CHECKING:
+    from .students import Student, Group
 
 
 # TODO UserBase, UserCreate, UserUpdate, UserPublic
@@ -8,8 +13,12 @@ class User(SQLModel, table=True):
     user_name: str = Field(index=True)
     hashed_password: str = Field()
     role_id: int | None = Field(foreign_key="role.id", ondelete="SET NULL")
+    student_id: int | None = Field(default=None, foreign_key="student.id", ondelete="SET NULL")
+    group_id: int | None = Field(default=None, foreign_key="group.id", ondelete="SET NULL")
 
     role: "Role" = Relationship(back_populates="users")
+    student: Optional["Student"] = Relationship(back_populates="user")
+    group: Optional["Group"] = Relationship(back_populates="user")
 
 
 class Role(SQLModel, table=True):
