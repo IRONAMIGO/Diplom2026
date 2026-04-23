a26cd2733cd45da97915ea064f770a52f0be50a0
user <sevcovnikita0@gmail.com>
2026-04-23 20:45:16 +0300

    Telemost

--- core/config.py
+++ core/config.py
@@ -6,3 +6,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # База данных
 DATABASE_URL = os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'data/app.db'}")
+
+
+TEST_VAR = os.getenv("TEST_VAR", "1111")
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-from core.config import BASE_DIR
+from core.config import BASE_DIR, TEST_VAR
 
 if __name__ == '__main__':
     print(BASE_DIR)
+    print(TEST_VAR)
